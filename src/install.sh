e1831b17f91e7b3ae5edb4880b20d89f237fbe77
TRex22 <contact@jasonchalom.com>
2016-09-04 14:41:01 +0200

    added web ui libraries

--- src/install.sh
+++ src/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo 'install websocketd for Linux'
 echo 'if you dont have root access the just extract websocket binary to this folder and use it here'
+#npm install -g bower
+#sudo npm install -g bower
 wget -c https://github.com/joewalnes/websocketd/releases/download/v0.2.12/websocketd-0.2.12-linux_amd64.zip 
 sudo unzip websocketd-0.2.12-linux_amd64.zip -d /usr/bin
