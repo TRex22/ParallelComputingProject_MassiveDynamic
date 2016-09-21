65f2f1a0dad2b154d87d5ac2c3a623e26eb52cf0
trex22 <trex22@jasonchalom.com>
2016-09-21 13:24:13 +0200

    still cleaning up

--- src/k_means_online.cpp
+++ src/k_means_online.cpp
@@ -35,40 +35,37 @@ void k_Means_Online(vector< vector<double> > data){
 		}
 	}
 
-			Print_Data("Data: ", data);
-			Print_Data("k: ", k);
-			Print_Data("Centres: ", centres);
+	Print_Data("Data: ", data);
+	Print_Data("k: ", k);
+	Print_Data("Centres: ", centres);
 
 	int it = 0;
-	while(it < 50){
-
-		for (int i = 0; i < data.size(); ++i){   //Finds nearest cluster centre and assigns a value in a vector corresponding to the closest centre
+	while(it < 50)
+	{
+		for (int i = 0; i < data.size(); ++i)
+		{   //Finds nearest cluster centre and assigns a value in a vector corresponding to the closest centre
 			int min_centre_index = 0;
 			cout << "Distance from data[" << i << "] to centre[0] is " << Find_Distance(data[i], centres[min_centre_index]) << endl;
-			for(int j = 1; j < centres.size(); ++j){
+			for(int j = 1; j < centres.size(); ++j)
+			{
 				cout << "Distance from data[" << i << "] to centre[" << j << "] is " << Find_Distance(data[i], centres[j]) << endl;
-				if( Find_Distance(data[i], centres[j]) < Find_Distance(data[i], centres[min_centre_index]) ){
+				if( Find_Distance(data[i], centres[j]) < Find_Distance(data[i], centres[min_centre_index]) )
+				{
 					min_centre_index = j;
 				}
 			}
 
-
-
-			for(int k = 0;k < centres[min_centre_index].size();k++){
+			for(int k = 0;k < centres[min_centre_index].size();k++)
+			{
 				centres[min_centre_index][k] = centres[min_centre_index][k] + learnRate * (data[i][k] - centres[min_centre_index][k]);
 			}
-
 		}
 
 		/*learnRate = 1/(it + 1);  //Potential extra stopping condition*/
-
-
 		it++;
 	}
 
-
 	Print_Data("Centres: ", centres);
-
 	/*Print_File("centres_online.csv", centres);*/
 }
 
